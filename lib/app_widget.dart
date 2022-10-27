2ef65ec642bb4c401cbfdd892294e8668074038e
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2022-10-27 12:52:16 -0300

    feat(login): create page

---
+++ lib/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:login_flutter/pages/login_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Login Flutter',
+      home: LoginPage(),
+    );
+  }
+}
